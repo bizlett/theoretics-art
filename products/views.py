ba123f8e6b7910ea55238776a8389683ef5d99f3
bizlett <bizzyday@gmail.com>
2022-03-22 07:20:31 +0000

    Added product view, url and template for product admin

--- products/views.py
+++ products/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
 
+from .models import Product, Category
+from .forms import ProductForm
+
+# Create your views here.
 
 def all_products(request):
-    """ A view to show all artwork for sale including sorting """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
     categories = None
     sort = None
     direction = None
@@ -24,17 +31,26 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
@@ -43,12 +59,23 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show all product details for available artwork """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    
+
     context = {
         'product': product,
     }
 
-    return render(request, 'products/product-detail.html', context)
+    return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
