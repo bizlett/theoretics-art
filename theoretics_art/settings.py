1fa0a7f6c5c19a2e838d4d31aa4964ac7197d8ee
bizlett <bizzyday@gmail.com>
2022-03-23 19:25:36 +0000

    Removed secret key and set debug

--- theoretics_art/settings.py
+++ theoretics_art/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^d(3b(qk&!jmb-x2882w^3v^un_v%-7322leu9cwv34+ca6&t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['theoretics-art.herokuapp.com', 'localhost']
 
