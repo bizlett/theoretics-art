d0a03e68d2ba5353d845fc98107637728dfef947
bizlett <bizzyday@gmail.com>
2022-04-03 12:30:30 +0000

    Added art and photography models into bag context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from products.models import Product, Art, Photography
+
 
 def bag_contents(request):
 
@@ -14,25 +15,39 @@ def bag_contents(request):
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            art = Art.objects.all()
+            photography = Photography.objects.all()
+
+            if product.category.name == "art":
+                for art_piece in art:
+                    total += item_data * art_piece.price
+
+            elif product.category.name == "photography":
+                for art_piece in photography:
+                    total += item_data * art_piece.price
+
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
+                'art': art,
+                'photography': photography,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
+            photography.objects.all()
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                total += quantity * photography.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
+                    'photography': photography,
                     'size': size,
                 })
-        
+    
     grand_total = delivery + total
 
     context = {
