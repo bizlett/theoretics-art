e8f242af30f0dfe895e8fddfe72217f91c7fc114
bizlett <bizzyday@gmail.com>
2022-04-04 18:35:45 +0000

    Rewrite bag context to display product info in bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,41 +12,27 @@ def bag_contents(request):
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     bag = request.session.get('bag', {})
 
-    for item_id, item_data in bag.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            art = Art.objects.all()
-            photography = Photography.objects.all()
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        art = Art.objects.all()
+        photography = Photography.objects.all()
 
-            if product.category.name == "art":
-                for art_piece in art:
-                    total += item_data * art_piece.price
-
-            elif product.category.name == "photography":
-                for art_piece in photography:
-                    total += item_data * art_piece.price
-
-            product_count += item_data
-            bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-                'art': art,
-                'photography': photography,
-            })
+        if product.category.name == "art":
+            for art_piece in art:
+                if art_piece.name == product.name:
+                    total += quantity * art_piece.price
         else:
-            product = get_object_or_404(Product, pk=item_id)
-            photography.objects.all()
-            for size, quantity in item_data['items_by_size'].items():
-                total += quantity * photography.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'photography': photography,
-                    'size': size,
-                })
+            if product.category.name == "photography":
+                for art_piece in photography:
+                    if art_piece.name == product.name:
+                        total += quantity * art_piece.price
+        
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
     
     grand_total = delivery + total
 
@@ -56,6 +42,8 @@ def bag_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'grand_total': grand_total,
+        'art': art,
+        'photography': photography,
     }
 
     return context
